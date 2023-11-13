4fc05bded316bd0945492d684a5a85d82602ec40
Maiconfz <maiconfz@gmail.com>
2023-11-13 23:31:37 +0000

    Add h2 dependency with datasource and data initialization

---
+++ src/main/java/com/my_app/exception/AppGenericException.java
@@ -0,0 +1,21 @@
+package com.my_app.exception;
+
+public class AppGenericException extends RuntimeException {
+	private static final long serialVersionUID = -8798179471741847116L;
+
+	public AppGenericException() {
+		super();
+	}
+
+	public AppGenericException(String message) {
+		super(message);
+	}
+
+	public AppGenericException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public AppGenericException(Throwable cause) {
+		super(cause);
+	}
+}
---
+++ src/main/java/com/my_app/home/db/DataSourceFactory.java
@@ -0,0 +1,19 @@
+package com.my_app.home.db;
+
+import javax.sql.DataSource;
+
+import org.h2.jdbcx.JdbcDataSource;
+
+public class DataSourceFactory {
+
+	public DataSource create() {
+		final JdbcDataSource ds = new JdbcDataSource();
+
+		ds.setURL("jdbc:h2:mem:my_app");
+		ds.setUser("sa");
+		ds.setPassword("sa");
+
+		return ds;
+	}
+
+}
