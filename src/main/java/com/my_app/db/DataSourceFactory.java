5cfd80034d5ebd2e844c1878e900dbe8208caa21
Maiconfz <maiconfz@gmail.com>
2023-11-14 00:36:02 +0000

    Fix data being lont on h2 memory db after last closed connection

--- src/main/java/com/my_app/db/DataSourceFactory.java
+++ src/main/java/com/my_app/db/DataSourceFactory.java
@@ -9,7 +9,7 @@ public class DataSourceFactory {
 	public DataSource create() {
 		final JdbcDataSource ds = new JdbcDataSource();
 
-		ds.setURL("jdbc:h2:mem:my_app");
+		ds.setURL("jdbc:h2:mem:my_app;DB_CLOSE_DELAY=-1");
 		ds.setUser("sa");
 		ds.setPassword("sa");
 
